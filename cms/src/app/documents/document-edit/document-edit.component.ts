b0fec6411b1922e083d0082508372eb8d6c790f5
lydiaswan15 <lydiaswan15@gmail.com>
2022-06-14 13:35:59 -0600

    started assignment, got stuck again, lol.

--- cms/src/app/documents/document-edit/document-edit.component.ts
+++ cms/src/app/documents/document-edit/document-edit.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup } from '@angular/forms';
+import { Document } from '../document.model';
+import { DocumentService } from '../document.service';
+import { Router } from '@angular/router';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-document-edit',
@@ -13,14 +17,24 @@ export class DocumentEditComponent implements OnInit {
   editMode: false;
 
 
-  constructor() {
-   }
+
+  constructor(
+    private documentService: DocumentService,
+    private router: Router,
+    private route: ActivatedRoute) {
+}
 
   ngOnInit(): void {
     console.log(this.document);
   }
 
-  onSubmit(form: FormGroup){}
+  onSubmit(form: FormGroup){
+    let value = form.value;
+    console.log(value);
+    let newDocument = new Document(1, value.name, value.description, value.url);
+    // How do we update the ID to be unique for each value?
+
+  }
   onCancel(){}
 
 }
