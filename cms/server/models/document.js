80dc6bf427823f8fab5461323eaf8a27339f7abb
lydiaswan15 <lydiaswan15@gmail.com>
2022-07-07 10:42:13 -0600

    ulr to url

--- cms/server/models/document.js
+++ cms/server/models/document.js
@@ -3,11 +3,11 @@ const mongoose = require('mongoose');
 const documentSchema = mongoose.Schema({
    id: { type: String, required: true },
    name: { type: String },
-   ulr: { type: String, required: true },
+   url: { type: String, required: true },
    children: [{
       id: { type: String, required: true },
       name: { type: String },
-      ulr: { type: String, required: true }
+      url: { type: String, required: true }
    }]
 });
 
